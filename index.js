c7d51f078e94e568fa145895ac26999cc5cec428
Ufuk Bakan <println.ufukbakan@gmail.com>
2023-02-28 15:17:10 +0300

    Revert "fix __dirname & __filename for es module"
    This reverts commit a81a1e13df0c54398c0d1fa4384777f099e0c820.

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 import fs from 'fs';
 import path from 'path';
-import { URL } from 'url';
-
-const __filename = new URL('', import.meta.url).pathname;
-const __dirname = new URL('.', import.meta.url).pathname;
 
 const parentFile = __filename;
 const parentDirectory = __dirname ?? path.dirname(parentFile || '.');
